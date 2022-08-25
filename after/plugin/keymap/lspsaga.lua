a96139162b92f9aa8f34b6aa066033a9d0079717
Andre Maeland <andre.maeland@netpower.no>
2022-08-25 08:41:23 +0200

    add lspsaga

---
+++ after/plugin/keymap/lspsaga.lua
@@ -0,0 +1,31 @@
+local Remap = require("am.keymap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+
+
+
+nnoremap('[e', '<Cmd>Lspsaga diagnostic_jump_next<CR>')
+nnoremap(']e', '<Cmd>Lspsaga diagnostic_jump_prev<CR>')
+nnoremap('K', '<Cmd>Lspsaga hover_doc<CR>')
+nnoremap('<leader>cd', '<cmd>Lspsaga show_line_diagnostics<CR>')
+-- code action 
+nnoremap('<leader>ca', '<Cmd>Lspsaga code_action<CR>')
+vnoremap('<leader>ca', '<cmd>Lspsaga range_code_action<CR>')
+
+nnoremap('gd', '<Cmd>Lspsaga lsp_finder<CR>')
+nnoremap('gp', '<Cmd>Lspsaga preview_definition<CR>')
+inoremap('<C-k>', '<Cmd>Lspsaga signature_help<CR>')
+
+-- rename
+nnoremap('<leader>rn', '<Cmd>Lspsaga rename<CR>')
+
+local action = require("lspsaga.action")
+-- scroll in hover doc or  definition preview window
+vim.keymap.set("n", "<C-f>", function()
+    action.smart_scroll_with_saga(1)
+end, { silent = true })
+-- scroll in hover doc or  definition preview window
+vim.keymap.set("n", "<C-b>", function()
+    action.smart_scroll_with_saga(-1)
+end, { silent = true })
